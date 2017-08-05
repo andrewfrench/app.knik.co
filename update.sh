eb193d11bbc5e23076bf91f080d575c522b92dc3
Andrew French <andrew@afren.ch>
2017-08-05 15:54:49 -0600

    Update update.sh for unified lambda.

--- update.sh
+++ update.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-INITIAL_PATH=`pwd`;
-export GOPATH=$INITIAL_PATH;
-UPDATE_PATH_PREFIX="$INITIAL_PATH/src/knik.co/api";
-UPDATE_PATH=$1;
-LAMBDA_NAME="lambda-unified";
-LAMBDA_STAGE="production";
+export GOPATH=`pwd`;
 
+LAMBDA="knikco-unified-production"
+LOCATION=`pwd`/src/knik.co/api/knikco-unified
 AWS_PROFILE="infill";
 AWS_REGION="us-west-2";
 
-echo "Updating $UPDATE_PATH-$LAMBDA_STAGE";
-
-echo "Moving to $UPDATE_PATH";
-cd $UPDATE_PATH_PREFIX/$UPDATE_PATH;
-
-echo "Copying Makefile to $UPDATE_PATH";
-cp $INITIAL_PATH/Makefile .;
+echo "Copying Makefile"
+cp ./Makefile ${LOCATION}/Makefile
 
 echo "Executing make";
+cd ${LOCATION}
 say "Build started";
 make
 
@@ -28,26 +21,23 @@ then
 	exit -1;
 fi
 
-echo "Removing copied Makefile";
-rm $UPDATE_PATH_PREFIX/$UPDATE_PATH/Makefile;
-
-if [ -s "$UPDATE_PATH_PREFIX/$UPDATE_PATH/handler.zip" ]; 
+if [ -s "${LOCATION}/handler.zip" ]; 
 then
 	echo "Build succeeded!";
 	say "Build succeeded";
 else
 	echo "Build failed!";
-	say "Build complete";
+	say "Build failed";
 	exit -1;
 fi
 
 say "Lambda update started";
-echo "Updating code for lambda: $LAMBDA_NAME";
+echo "Updating code for lambda: $LAMBDA";
 aws lambda update-function-code \
 	--profile $AWS_PROFILE \
 	--region $AWS_REGION \
-	--function-name $LAMBDA_NAME-$LAMBDA_STAGE \
-	--zip-file "fileb://$UPDATE_PATH_PREFIX/$UPDATE_PATH/handler.zip";
+	--function-name $LAMBDA \
+	--zip-file "fileb://${LOCATION}/handler.zip";
 
 if [ $? -ne 0 ];
 then
@@ -59,8 +49,5 @@ else
 fi 
 
 echo "Cleaning up";
-# rm $UPDATE_PATH_PREFIX/$UPDATE_PATH/handler.zip;
-rm $UPDATE_PATH_PREFIX/$UPDATE_PATH/handler.so;
+rm ${LOCATION}/handler.so ${LOCATION}/Makefile
 
-echo "Returning to initial path";
-cd $INITIAL_PATH;
